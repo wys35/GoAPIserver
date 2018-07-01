120f53dbea19b90fd3bdfb97bed27c16de65e748
Eddy Wu <wuyuansheng@gmail.com>
2018-07-01 19:33:38 +0800

    server running

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+var router *gin.Engine
+
 func main() {
-	fmt.Println("Hello World.")
+	router = gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+	router.Run()
 }
