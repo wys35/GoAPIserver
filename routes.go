ebc3426fbb32c8cca448b29ba87477ae8dae0231
Eddy Wu <wuyuansheng@gmail.com>
2018-07-02 15:15:39 +0800

    working code

--- routes.go
+++ routes.go
@@ -2,4 +2,9 @@ package main
 
 func initializeRoutes() {
 	router.GET("/", showIndexPage)
+
+	articleRoutes := router.Group("/article")
+	{
+		articleRoutes.GET("/view/:article_id", getArticle)
+	}
 }
