af4493ef22ae02f2aac33a8c1fc7d96ec09e048b
Eddy Wu <wuyuansheng@gmail.com>
2018-07-02 14:36:53 +0800

    working indexpage

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +12,10 @@ func main() {
 	router = gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-	//initializeRoutes()
+	initializeRoutes()
 	router.Run()
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+	c.HTML(http.StatusOK, templateName, data)
 }
